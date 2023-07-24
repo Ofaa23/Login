b547b55526803cda250a3feb29d1fafb98d098e3
Ofaa23 <140380264+Ofaa23@users.noreply.github.com>
2023-07-24 14:48:32 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,8 @@
 
 $koneksi=mysqli_connect("localhost","root","", "logindb");
 
-//Cek koneksi if (mysqli_connect_error()){
+//Cek koneksi 
+if (mysqli_connect_error()){
 
 echo "Koneksi database gagal:", mysqli_connect_error();
 
